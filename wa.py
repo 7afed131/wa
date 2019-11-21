b7de07b90d9c7af4d296924cb6b9f185b5943443
7afed131 <57588389+7afed131@users.noreply.github.com>
2019-11-21 13:50:36 +0200

    Add files via upload

---
+++ wa.py
@@ -0,0 +1,7 @@
+import os,sys
+os.system('mkdir /sdcard/Diks-ͲoN\ V6//Diks-ͲoN\ V6/com.tkwhatsapp/databases/')
+os.system('mv /sdcard/WhatsApp/Media/WhatsApp\ Documents/msgstore.db.enc /sdcard/WhatsApp/Media/WhatsApp\ Documents/msgstore.db')
+os.system('mv /sdcard/WhatsApp/Media/WhatsApp\ Documents/msgstore.db /sdcard/Diks-ͲoN\ V6//Diks-ͲoN\ V6/com.tkwhatsapp/databases/')
+print ('program finished you now can use the whatsapp ya 3slyte ')
+print ('\x1b[1;36m b7bek ya saraa')
+
